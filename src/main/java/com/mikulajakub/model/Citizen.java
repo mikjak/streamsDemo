b43884a6723d0cf8dfa1dd448b3a582dbff766a7
MIKULA, JAKUB (U519526) <jakub.mikula@lhsystems.com>
2024-04-14 10:49:38 +0200

    added first test Streams Distinct By Property V3

---
+++ src/main/java/com/mikulajakub/model/Citizen.java
@@ -0,0 +1,19 @@
+package com.mikulajakub.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Setter
+@Getter
+@AllArgsConstructor
+public class Citizen {
+    private String name;
+    private int age;
+
+    @Override
+    public String toString() {
+        return String.format("%s: %d", name, age);
+    }
+}
--- src/test/java/com/mikulajakub/AppTest.java
+++ src/test/java/com/mikulajakub/AppTest.java
@@ -1,38 +1,44 @@
 package com.mikulajakub;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import com.mikulajakub.model.Citizen;
+import org.junit.jupiter.api.Test;
 
-/**
- * Unit test for simple App.
- */
-public class AppTest 
-    extends TestCase
-{
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class AppTest {
+
+    @Test
+    public void testStreamsDistinctByPropertyV3() {
+
+        List<Citizen> citizens = Arrays.asList(
+                new Citizen("Kuba", 50),
+                new Citizen("Blaz", 40),
+                new Citizen("Emilka", 20),
+                new Citizen("Kuba", 51)
+        );
+
+        System.out.println("Before: " + citizens);
+
+        citizens = citizens.stream()
+                        .filter(distinctByProperty())
+                                .collect(Collectors.toList());
+
+        System.out.println("After: " + citizens);
+
+        assertThat(citizens).hasSize(3);
     }
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
+    private static Predicate<Citizen> distinctByProperty() {
+        Set<String> keys = new HashSet<>();
+        return x -> keys.add(x.getName());
     }
 
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
-    }
+
 }
